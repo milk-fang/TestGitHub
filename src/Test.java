32cad98e3800cda482dde0fda8f3c23e8bc7a09e
zhaoge <1993742721@qq.com>
2020-10-25 20:15:47 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * 类描述
+ *
+ * @ClassName Test
+ * @Description TODO
+ * @Author vip
+ * @Date 2020/10/25 20:12
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
